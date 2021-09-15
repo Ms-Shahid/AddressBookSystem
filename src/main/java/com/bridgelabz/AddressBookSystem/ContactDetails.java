26b5f9f055217b6273ce302be0d05a6b2e2a1277
Ms-Shahid <ms2406shahid@gmail.com>
2021-09-15 14:34:21 +0530

    Use case 1 to take contact details from user

--- src/main/java/com/bridgelabz/AddressBookSystem/ContactDetails.java
+++ src/main/java/com/bridgelabz/AddressBookSystem/ContactDetails.java
@@ -2,28 +2,22 @@ package com.bridgelabz.AddressBookSystem;
 
 public class ContactDetails {
 
-	private String firstName,lastName,address;
-	private int zipCode,phoneNumber;
-    private String email;
-    
-	public ContactDetails(String firstName, String lastName, String address,int zipCode, int phoneNumber,String email) {
+	private String firstName, lastName, address;
+	private int zipCode, phoneNumber;
+	private String email;
+
+	public ContactDetails(String firstName, String lastName, String address, int zipCode, int phoneNumber,
+			String email) {
 		this.firstName = firstName;
-    	this.lastName = lastName;
-    	this.address = address;
-    	this.email = email;
-    	this.zipCode = zipCode;
-    	this.phoneNumber = phoneNumber;
+		this.lastName = lastName;
+		this.address = address;
+		this.email = email;
+		this.zipCode = zipCode;
+		this.phoneNumber = phoneNumber;
 	}
-	
+
 	public void disp() {
-		System.out.println(
-		"\n Your Name is " + firstName +" " + lastName +
-		"\n address " + address +
-		"\n ZipCode " + zipCode +
-		"\n Phone Number " + phoneNumber +
-		"\n Email " + email 
-		+ "*****************");
+		System.out.println("\n Your Name is " + firstName + " " + lastName + "\n address " + address + "\n ZipCode "
+				+ zipCode + "\n Phone Number " + phoneNumber + "\n Email " + email + "*****************");
 	}
-	
-	
 }
