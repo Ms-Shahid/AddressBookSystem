cb504111ef2855b2cc5053e3fe6600e6eeaf0e0f
Ms-Shahid <ms2406shahid@gmail.com>
2021-09-15 14:31:01 +0530

    adding contact details to addressbook

---
+++ src/main/java/com/bridgelabz/AddressBookSystem/App.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.AddressBookSystem;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App 
+{
+	static ArrayList<ContactDetails> List = new ArrayList<>();
+	
+    public static void main( String[] args ){
+    	
+    	addContacts();
+        
+    }
+    
+    public static void addContacts() {
+    	
+    	Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Your FirstName: ");
+        String firstName = scan.nextLine();
+        
+        System.out.println("Enter the Your LastName: ");
+        String lastName = scan.nextLine();
+        
+        System.out.println("Enter the Your Address: ");
+        String address = scan.nextLine();
+        
+        System.out.println("Enter the Your ZipCode: ");
+        int zipCode = scan.nextInt();
+        
+        System.out.println("Enter the Your Phone Number: ");
+        int phoneNumber = scan.nextInt();
+        
+        System.out.println("Enter the Your Email: ");
+        String email = scan.next();
+        
+        System.out.println("****Success****");
+        ContactDetails userDetails = new ContactDetails(firstName,lastName,address,zipCode,phoneNumber,email);
+        userDetails.disp();;
+        
+        scan.close();
+    }
+}
